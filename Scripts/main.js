7bc37ae87591a882cc30febaf7dd3ebb08b6f8b6
urielzen <urielzen@yahoo.com>
2015-07-20 00:03:49 -0500

    first commit

---
+++ Scripts/main.js
@@ -0,0 +1,139 @@
+﻿"use strict"
+var chatApp = {};
+chatApp.messages = [];
+chatApp.urlBase = "https://junior-coder-chat.firebaseio.com/"
+
+chatApp.ajaxCall = function (verb, myObj, success, dir) {
+    var request = new XMLHttpRequest();
+    dir = dir || "";
+    request.open(verb, chatApp.urlBase + dir + ".json", true)
+    request.onload = function () {
+        if (this.status >= 200 && this.status < 400) {
+            success(this.response);
+        } else {
+            alert("Error" + this.response)
+        }
+    }
+    request.onerror = function () {
+        alert("Error on communication");
+    }
+    if (myObj) {
+        request.send(JSON.stringify(myObj));
+    } else {
+        request.send();
+    }
+}
+
+chatApp.drawChat = function () {
+    var holder = "";
+    for (var i = 0; i < chatApp.messages.length; i++) {
+        holder += "<img width='60' height='40' src='" + chatApp.messages[i].pic + "'/>" +
+           chatApp.mySuperDate(chatApp.messages[i].date) + " "
+        holder += chatApp.messages[i].user ? chatApp.messages[i].user : " "
+        holder += " says..." +
+            "<button onclick='chatApp.updateMsg(" + '"' + chatApp.messages[i].key + '"' + ")'>Edit</button> " +
+            "<button onclick='chatApp.deleteMsg(" + '"' + chatApp.messages[i].key + '"' + ")'>Delete</button>" +
+            "</br>" +
+            chatApp.messages[i].message + "</br></br>";
+    }
+    document.getElementById("mainWindowInner").innerHTML = holder;
+    setTimeout(function () {
+        $("#mainWindow").mCustomScrollbar("scrollTo", "bottom");
+    }, 0)
+}
+
+
+chatApp.sendMsg = function () {
+    var message = document.getElementById("message").value;
+    var user = document.getElementById("user").value;
+    var pic = document.getElementById("img").value;
+    var myDate = new Date();
+    var myObj = {
+        message: message,
+        user: user,
+        pic: pic,
+        date: myDate
+    };
+    chatApp.ajaxCall("POST", myObj, function (respuesta) {
+        chatApp.receiveMsg();
+    });
+    document.getElementById("message").value = "";
+}
+chatApp.receiveMsg = function () {
+    chatApp.ajaxCall("GET", null, function (respuesta) {
+        var data = JSON.parse(respuesta);
+        chatApp.messages = [];
+        for (var prop in data) {
+            data[prop].key = prop
+            chatApp.messages.push(data[prop])
+        }
+        chatApp.drawChat();
+    });
+}
+chatApp.updateMsg = function (key) {
+    var message = document.getElementById("message").value;
+    var user = document.getElementById("user").value;
+    var pic = document.getElementById("img").value;
+    var myObj = {
+        message: message,
+        user: user,
+        pic: pic
+    };
+
+    chatApp.ajaxCall("PATCH", myObj, function (respuesta) {
+        chatApp.receiveMsg();
+    }, key)
+}
+chatApp.deleteMsg = function (key) {
+    chatApp.ajaxCall("DELETE", null, function (respuesta) {
+        chatApp.receiveMsg();
+    }, key)
+}
+
+chatApp.convertMonth2 = function (monthNumber) {
+    var myMonths = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+    return myMonths[monthNumber];
+}
+chatApp.mySuperDate = function (dateFromDB) {
+    var currentTime = new Date()
+    var dBTime = new Date(dateFromDB)
+    var difference = currentTime - dBTime;
+    var difference2;
+    var minLeft;
+    if (difference < 24 * 60 * 60 * 1000) {
+        difference2 = difference / (60 * 60 * 1000)
+        minLeft = (difference2 - (difference2 | 0)) * 60;
+        return (difference2 | 0) + "Hours " + (minLeft | 0) + "minutes ago";
+    } else {
+        var amVSpm;
+        var hours = dBTime.getHours()
+        if (hours < 12) {
+            amVSpm = "am";
+        } else {
+            hours = hours - 12;
+            amVSpm = "pm";
+        }
+        var fixDate = chatApp.convertMonth2((dBTime).getMonth()) + " " +
+                   dBTime.getDate() + ", " +
+                   dBTime.getFullYear() + " " +
+                   hours + ":" +
+                   dBTime.getMinutes() + amVSpm;
+        return fixDate;
+    }
+}
+
+chatApp.enterPressed = function (e) {
+    if (e.charCode === 13 || e.keyCode === 13) {
+        chatApp.sendMsg();
+    }
+}
+
+chatApp.receiveMsg();
+
+setInterval(function () {
+    chatApp.receiveMsg();
+}, 5000)
+
+setTimeout(function () {
+    $("#mainWindow").mCustomScrollbar({ theme: "dark-thick" });
+}, 0)
---
+++ Scripts/main2.js
@@ -0,0 +1,144 @@
+﻿"use strict"
+var app = angular.module("chatApp", ["ngRoute"])
+
+app.config(function ($routeProvider) {
+    $routeProvider.when("/", {
+        templateUrl: "Views/chat.html",
+        controller: "chatController"
+    })
+    .otherwise({
+        template: "You've reached a page that doesn't exist!"
+    })
+})
+
+app.factory("dBFactory", function () {
+    return [
+        {
+            date: new Date(2015, 6, 17),
+            message: "Hello coders",
+            pic: "https://",
+            user: "Wen"
+        }, {
+            date: new Date(2015, 6, 17),
+            message: "Hello you!",
+            pic: "https://",
+            user: "Ben"
+        }, {
+            date: new Date(2015, 6, 17),
+            message: "Best day to code!",
+            pic: "https://",
+            user: "Len"
+        }
+    ]
+})
+
+app.filter("myDate", [function () {
+    return function (dateFromDB) {
+        var currentTime = new Date()
+        var dBTime = new Date(dateFromDB)
+        var difference = currentTime - dBTime;
+        var difference2, minLeft;
+        if (difference < 24 * 60 * 60 * 1000) {
+            difference2 = difference / (60 * 60 * 1000);
+            minLeft = (difference2 - (difference2 | 0)) * 60;
+            return (difference2 | 0) + "Hours " + (minLeft | 0) + "minutes ago";
+        } else {
+            var amVSpm, hours = dBTime.getHours()
+            if (hours < 12) {
+                amVSpm = "am";
+            } else {
+                hours = hours - 12;
+                amVSpm = "pm";
+            }
+            var myMonths = ["Jan", "Feb", "Mar", "Apr", "May", "Jun",
+                            "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+            var fixDate = myMonths[(dBTime).getMonth()] + " " +
+                       dBTime.getDate() + ", " +
+                       dBTime.getFullYear() + " " +
+                       hours + ":" +
+                       dBTime.getMinutes() + amVSpm;
+            return fixDate;
+        }
+        return "asdfasdf"
+    }
+}])
+
+app.controller("chatController", ["$scope", "dBFactory", "$http", "$interval", "$timeout", function ($scope, dBFactory, $http, $interval, $timeout) {
+    $scope.messages = dBFactory;
+    $scope.url = "http://cdn.meme.am/instances/500x/58595388.jpg";
+    $scope.sendMsg = function () {
+        var obj = JSON.stringify({
+            user: $scope.name,
+            message: $scope.msg,
+            pic: $scope.url,
+            date: new Date()
+        });
+        $http.post("https://junior-coder-chat.firebaseio.com/.json", obj)
+        .success(function () {
+            $scope.getMsg();
+        })
+        .error(function () {
+            console.log("errorrr")
+        })
+        $scope.msg = "";
+    }
+
+    $scope.getMsg = function () {
+        $http.get("https://junior-coder-chat.firebaseio.com/.json")
+        .success(function (data) {
+            $scope.messages = [];
+            for (var prop in data) {
+                data[prop].key = prop;
+                $scope.messages.push(data[prop])
+            }
+            $timeout(function () {
+                $("#mainWindow2").mCustomScrollbar("scrollTo", "bottom");
+            }, 0)
+        })
+        .error(function () {
+            console.log("errorrr")
+        })
+    }
+
+    $scope.deleteMsg = function (key) {
+        $http.delete("https://junior-coder-chat.firebaseio.com/" + key + "/.json")
+        .success(function () {
+            $scope.getMsg();
+        })
+        .error(function () {
+            console.og("Error");
+        })
+    }
+
+    $scope.editMsg = function (key) {
+        var obj = JSON.stringify({
+            user: $scope.name,
+            message: $scope.msg,
+            pic: $scope.url,
+            date: new Date()
+        });
+        $http.patch("https://junior-coder-chat.firebaseio.com/" + key + "/.json", obj)
+        .success(function () {
+            $scope.getMsg();
+        })
+        .error(function () {
+            console.log("Error")
+        })
+    }
+
+    $scope.enterPressed = function (e) {
+        if (e.keyCode === 13 || e.charCode === 13) {
+            $scope.sendMsg();
+        }
+    }
+
+    $scope.getMsg();
+
+    $interval(function () {
+        $scope.getMsg();
+    }, 5000)
+
+    $timeout(function () {
+        $("#mainWindow2").mCustomScrollbar({ theme: "dark-thick" });
+    }, 0)
+}])
